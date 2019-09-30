87e761ed76402c5ca1f8c80663a7ae6cc2b0ca64
chaoy <15637237221@163.com>
2019-09-30 16:12:53 +0800

    springboot启动类进行了更改，可以打包成war

--- src/main/java/com/yh/YhApplication.java
+++ src/main/java/com/yh/YhApplication.java
@@ -3,12 +3,18 @@ package com.yh;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.yh.mapper")
-public class YhApplication {
+public class YhApplication extends SpringBootServletInitializer {
 
-    public static void main (String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(YhApplication.class);
+    }
+    public static void main(String[] args) {
         SpringApplication.run(YhApplication.class, args);
     }
 
