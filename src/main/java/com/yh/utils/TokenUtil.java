eace13c282bef36e4c639aa362f58cf66e2086d2
maczyz <824640188@qq.com>
2019-09-30 11:11:14 +0800

    TokenUtil：传 user_id，返回 token 值（token 值里的英文字母全部大写）

--- src/main/java/com/yh/utils/TokenUtil.java
+++ src/main/java/com/yh/utils/TokenUtil.java
@@ -1,5 +1,9 @@
 package com.yh.utils;
 
+import sun.tools.jstat.Token;
+
+import java.util.Date;
+
 /**
  * 请后来修改者按照这个格式添加备注
  * 示例：为了第一次推送，未添加任何数据
@@ -7,4 +11,19 @@ package com.yh.utils;
  * @date 2019/9/29
  */
 public class TokenUtil {
+
+    public String TokenProcessor(String user_id){
+
+        String nowTime = new Date().toString();
+        String token = MD5Utils.md5(user_id + nowTime).toUpperCase();
+        return token;
+    }
+
+
+
+
+
+
+
+
 }
