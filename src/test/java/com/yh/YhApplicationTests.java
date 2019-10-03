d547271263e0bfc1e0c19ca5f513ac0bc8b8c5db
chaoy <15637237221@163.com>
2019-10-03 17:36:40 +0800

    去除test

--- src/test/java/com/yh/YhApplicationTests.java
+++ src/test/java/com/yh/YhApplicationTests.java
@@ -1,36 +1,17 @@
 package com.yh;
 
-import com.yh.entity.VideoCourse;
-import com.yh.service.DraftService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class YhApplicationTests {
-    @Autowired
-    private DraftService DraftService;
-
-
-    /**
-     * 测试Draft,有问题，待纠正
-     *
-     * @author rongjing
-     * @date 2019/10/03
-     */
 
     @Test
     public void contextLoads() {
-
-
-        VideoCourse course = DraftService.findByVideoCourseId("2");
-
-        System.out.println(course.getVideoInfo());
-
-
+        
     }
 
 }
