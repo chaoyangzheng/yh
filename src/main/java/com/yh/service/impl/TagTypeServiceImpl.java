beee678c338f246c924c80f77e5b7860c31e35ef
sgy <631407812@qq.com>
2019-10-05 15:41:17 +0800

    19/10/5时光熠下午第一版，添加查询标签列表功能

---
+++ src/main/java/com/yh/service/impl/TagTypeServiceImpl.java
@@ -0,0 +1,28 @@
+package com.yh.service.impl;
+
+import com.yh.entity.TagType;
+import com.yh.mapper.TagTypeMapper;
+import com.yh.service.TagTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author SHIGUANGYI
+ * @date 2019/10/5
+ */
+@Service
+public class TagTypeServiceImpl implements TagTypeService {
+    @Autowired
+    private TagTypeMapper tagTypeMapper;
+
+    /**
+     * @author SHIGUANGYI
+     * @date 2019/10/5
+     */
+    @Override
+    public List<TagType> findAllTagType() {
+        return null;
+    }
+}
